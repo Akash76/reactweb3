8269148b9e2763fe06d165f4163c1430e5390eb2
Akash Chekka <akashchekka@microsoft.com>
2022-05-19 14:24:51 +0530

    removing react

--- README.md
+++
@@ -1,3 +0,0 @@
-# reactweb3
-
-This repository contains code for React integrated with web3 hook useDapp.
--- src/components/UnauthenticatedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { useAppContext } from "../utils/context";
-
-export default function UnauthenticatedRoute({ children }) {
-  const { userAuthenticated } = useAppContext();
-
-  return !userAuthenticated ? children : <Navigate to="/dashboard" replace/>
-}
