3b709608276bc3f3a37d9522b2d3e4a76d6e62c8
Akash Chekka <akashchekka@microsoft.com>
2022-06-28 19:41:02 +0530

    added toaster code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,8 @@
 import abi from './Greeter.json';
+import toasterAbi from './Toaster.json';
 
 export const contractAddress = "0x810Dc166CF07Aa3386DA8AfBdDEB18Ec057c8C6b";
 export const ABI = abi.abi;
+
+export const toasterContractAddress = "0x22830681102aB1c6B1AfA15eEa3dB861BFE170C1";
+export const ToastABI = toasterAbi.abi;
